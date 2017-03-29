8d674bcc66d4e74eddf61046394fc9b3fb6c84f4
Denis Korniychuk <new79den@gmail.com>
2017-03-29 22:29:34 +0300

    first stap for include images

--- src/app/landing-pages/landing-pages.module.ts
+++ src/app/landing-pages/landing-pages.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './landing-page/header/header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LandingPageComponent]
+  declarations: [LandingPageComponent, HeaderComponent]
 })
 export class LandingPagesModule { }
