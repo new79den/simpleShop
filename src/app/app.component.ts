2736500383f5e370f096da48cf8cbaf12e1278fd
Denis Korniychuk <new79den@gmail.com>
2017-03-29 20:36:10 +0300

    go to start https://www.youtube.com/watch?v=_hnfJOnP_pk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ss-root',
   templateUrl: './app.component.html',
-  styleUrls: ['app.component.less']
 })
 export class AppComponent {
   title = 'ss works!';
--- src/app/landing-pages/landing-page/landing-page.component.ts
+++ src/app/landing-pages/landing-page/landing-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ss-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['less/base.less',  'less/help-classes.less', 'less/icons.less', 'less/mixins.less', 'less/variables.less', 'less/intergace.less', 'less/fonts.less']
+  styleUrls: ['landing-page.component.less']
 })
 export class LandingPageComponent implements OnInit {
 
