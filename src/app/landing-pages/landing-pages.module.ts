b4de741d3ff413c66967e8c24910d2f10a2976ce
Denis Korniychuk <new79den@gmail.com>
2017-03-30 21:20:50 +0300

    create section uslug

--- src/app/landing-pages/landing-pages.module.ts
+++ src/app/landing-pages/landing-pages.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HeaderComponent } from './landing-page/header/header.component';
+import { SectionComponent } from './landing-page/section/section.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LandingPageComponent, HeaderComponent]
+  declarations: [LandingPageComponent, HeaderComponent, SectionComponent]
 })
 export class LandingPagesModule { }
